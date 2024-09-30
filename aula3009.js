a7c1dd2fe3798662d0783ea1d2c0b0db40c51552
luangst <luisaangstmoncalves13@gmail.com>
2024-09-30 11:41:55 -0300

    Add files via upload

---
+++ aula3009.js
@@ -0,0 +1,48 @@
+/*let a = Number(prompt('digite um numero'))
+let b = Number(prompt("digite outro numero"))
+// não são iguais por isso o programa não vai aparecer,se for igual aparece
+
+function numeros(num1, num2){
+if (num1 === num2){
+    console.log("São iguais!")
+}else if(num1 >num2) {//com o else aparece se não aparecer pelo if
+    console.log("Num1 é maior que o Num2!")
+}else{
+    console.log("Num1 é menor que Num2!")
+}
+}
+numeros(a , b)*/
+
+
+
+/*let pokémon= prompt("Escolha um pokémon")
+switch (pokémon){
+ case 'Bulbasauro':
+    console.log('Água e veneno')
+    break
+ case 'Charmander':
+    console.log('Fogo')
+    break
+ case 'Squirtle':
+    console.log('Água')
+    break
+default:
+    console.log("Pokémon não encontrado")
+    break
+}*/
+
+
+
+/*let conclusão= prompt('Concluiu o ensino médio?')
+let deMaior= Number(prompt('Qual a sua idade?'))
+let outra= prompt('Você esta cursando outra faculdade')
+
+function pessoa(conclusão, deMaior, outra){
+if (conclusão==='sim' && deMaior>=18 && outra==='não'){
+    console.log("Pode estudar aqui!")
+}else {
+    console.log("Não podera estudar aqui")
+}
+}
+pessoa(conclusão, deMaior, outra)*/
+
