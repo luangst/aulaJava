55ab6db8c815360359f34734a97d8651917d7afa
luangst <luisaangstmoncalves13@gmail.com>
2024-10-02 11:46:30 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,109 +1,38 @@
-/*const respostaDoUsuario = prompt("Digite o número que você quer testar")
-const numero = Number(respostaDoUsuario)
+/*let numeros = Number(prompt('insira um numer ou 0 para sair'))
+let soma = 0
 
-if (numero % 2 === 0) {
-  console.log("Passou no teste.")
-} else {
-  console.log("Não passou no teste.")
+while(numeros !== 0){//diferente de zero
+ numeros = Number(prompt('insira um numer ou 0 para sair'))
+  soma = soma + numeros
 }
-//a)o código faz o calculo para ver se o numero colocado pelo usuario tem o resto 0 após a divisão por 2
-//b)para os numeros que quando divididos por 2 ficam com o reto igual a 0
-//c)para numeros que não tem resto 0*/
+console.log(soma)*/
+//enquanto a codição for verdadeira vai ser repetido
 
 
 
-/*let fruta = prompt("Escolha uma fruta")
-let preco
-switch (fruta) {
-  case "Laranja":
-    preco = 3.5
-    break;
-  case "Maçã":
-    preco = 2.25
-    break;
-  case "Uva":
-    preco = 0.30
-    break;
-  case "Pêra":
-    preco = 5.5
-    break; // BREAK PARA O ITEM c.
-  default:
-    preco = 5
-    break;
+/*let lista=[10,39,16,24,26]
+let mN= 0
+function maiorNumero(array){
+ for(let i= 0; i<array.length; i++){
+if(mN<lista[i]){
+mN = lista[i]
 }
-console.log("O preço da fruta ", fruta, " é ", "R$ ", preco)
-//a)para dizer o valor das frutas que estão citadas acima,default para se a fruta não for encontrada
-//b)"O preço da fruta Maça é R$2,25"
-//c)imprimiu normal a mensagem de que a Pêra é 5,50*/
-
-
-
-/*const numero = Number(prompt("Digite o primeiro número."))
-		
-if(numero > 0) {
-  console.log("Esse número passou no teste")
-	let mensagem = "Essa mensagem é secreta!!!"
 }
-
-console.log(mensagem)
-//a)Esta perguntando ao usuario um numero
-//b)(10)Esse numero passou no teste e diz que mensagem não é declarada(Porém se colocar o mouse em cima da mensagem dentro do if aparece essa mensagem é declarada mas não sera lida).(-10)não sera nada impresso
-//c)o codigo dentro do if só é executado se a condição é verdadeira(bloco).por conta da variavel estar dentro do if não pode ser acessada fora dele (escopo)*/
-
-
-
-/*let idade= Number(prompt("Qual a sua idade?"))
-
-function dirigir(idade){
-  if (idade>=18) {
-    console.log('Você pode dirigir')
-  }  else{
-    console.log('Você não pode dirigir')
-  }
 }
-dirigir(idade)*/
+maiorNumero(lista)
+console.log(mN)*/
 
 
 
-/*let horario= prompt("para os seu turno de aula digite M,V ou N.")
+let palavras= ["Bom","dia",",como","você","esta?"]
+let frase = ''
 
-function escola(horario){
-    if(horario==='M'){
- console.log("Bom Dia")
-    }else if (horario==='V'){
- console.log("Boa Tarde")
-    }if (horario==='N'){
- console.log("Boa Noite")
-    }
-}
-escola(horario)
-
-
-switch(horario){
-    case 'M':
-        console.log('Bom Dia')
-        break
-    case 'V':
-        console.log('Boa Tarde')
-        break
-    case 'N':
-        console.log('Boa Noite')
-       break
-}*/
-
-
-/*let gFilme= prompt('Qual o gênero de filme?')
-let ingresso= Number(prompt('Valor do ingresso?'))
-//desafio
-let lanche= prompt('Qual snack você vai comprar?')
-
-function assistir(gFilme,ingresso){
- if(gFilme==='fantasia' && ingresso<15){
- console.log(`Bom Filme e Aproveite o seu ${lanche}`)
- }else{
-    console.log('Escolha outro filme :(')
+function imprimir(palavras){
+ for(let palavra of palavras){
+  frase = frase + " " +palavra
+ //pra fazer uma string de espaço, colocar aspas e dar espaço no meio delas
  }
 }
-assistir(gFilme,ingresso)*/
+imprimir(palavras)
+console.log(frase)
 
-aula dia 01/10
